3e7351e2b5a6db8a060d69613d324a8089d8fddf
nicobengamma <kikecarc17@gmail.com>
2022-11-29 12:55:01 -0300

    ,..,.,

--- models/model.js
+++ models/model.js
@@ -1,8 +1,32 @@
 const uuid = require("uuid");
 
 class Products {
-  constructor(id, { name, age }) {
-    (this.id = id), (this.name = name);
-    this.age = age;
+  constructor(id, { name, descripcion, codigo, price, stock, url }) {
+    this.id = id;
+    this.name = name;
+    this.descripcion = descripcion;
+    this.codigo = codigo;
+    this.price = price;
+    this.stock = stock;
+    this.url = url;
   }
 }
+
+const db = {};
+
+function getProductos({ id }) {
+  if (!db[id]) {
+    throw new Error("Productos no exists");
+  }
+  return db;
+}
+
+function createProducto({ data }) {
+  const id = uuid.v1();
+  const newProducto = new Products(id, data);
+  db[id] = newProducto;
+
+  return newProducto;
+}
+
+function updateProducto() {}
